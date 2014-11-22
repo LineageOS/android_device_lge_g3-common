8541e2677ff045ab28ed7ca984b0076793cf1ea9
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-22 13:55:44 -0600

    sepolicy: add services to domains
    * TODO:
      Service su_daemon needs a SELinux domain defined; please fix!
    
    Change-Id: I9f4c68e63a7926781800884853ab3d1b882797f2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,5 +116,13 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Radio
 BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
 
+# SELinux
+BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    qrngd.te \
+    sysinit.te
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
