e7e692019246714db2ec2a0b6f421dd4f39e085d
invisiblek <dan.pasanen@gmail.com>
2014-12-18 21:45:35 -0600

    sepolicy: Clean up duplicate SEPOLICY_UNION members
    Change-Id: Ib34c7921a925cc3387e0f4b5fd7b03d08df62b8a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,34 +127,9 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    device.te \
-    file.te \
-    file_contexts \
-    init_shell.te \
-    genfs_contexts \
-    hostapd.te \
     kcal_dev.te \
-    kernel.te \
     lge_touch_sysfs.te \
-    mm-pp-daemon.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    nfc.te \
-    platform_app.te \
-    property.te \
-    property_contexts \
-    rmt_storage.te \
-    sensors.te \
-    servicemanager.te \
-    sysinit.te \
-    system_app.te \
-    tee.te \
-    thermal-engine.te \
-    time_data_file.te \
-    ueventd.te \
-    vold.te \
-    wcnss_service.te \
-    wpa.te
+    time_data_file.te
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
