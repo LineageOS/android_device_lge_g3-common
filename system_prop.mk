da626d62f08e5f67822fa80d160fdaaf5e01977b
AndroidUser00110001 <androiduser00110001@gmail.com>
2014-12-23 08:29:32 -0500

    VoIP Fix
    Change-Id: I45233ad0fc89974a7d214b2afee543d548a1007e

--- system_prop.mk
+++ system_prop.mk
@@ -23,7 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    use.dedicated.device.for.voip=true \
+    use.dedicated.device.for.voip=false \
     use.voice.path.for.pcm.voip=true
 
 # Display
