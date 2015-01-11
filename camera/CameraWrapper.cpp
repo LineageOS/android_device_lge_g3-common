0a71ee5cf09444e76ea3750e176a2868de073823
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-11 15:08:44 -0600

    camera: set lge-camera parameter
    Change-Id: I83533a3d4e093b2ed9225ed52f760a787c74e7b7

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -176,6 +176,8 @@ static char *camera_fixup_setparams(int id, const char *settings)
     params.dump();
 #endif
 
+    params.set(android::CameraParameters::KEY_LGE_CAMERA, "1");
+
     if (params.get(android::CameraParameters::KEY_RECORDING_HINT)) {
         videoMode = (!strcmp(params.get(android::CameraParameters::KEY_RECORDING_HINT), "true"));
     }
