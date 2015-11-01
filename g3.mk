eaa6598f51b0704e719b58a47bd352c8e69a15f0
Adam Farden <adam@farden.cz>
2015-11-01 22:13:22 +0100

    g3: Use open source thermanager
    The current thermal solution spams the shit out of logcat
    and doesn't really work anyway. Sony's thermanager is open
    and proven to be effective.
    
    This config is based on Xperia Z3 config, but with limits
    approximately +10'C to compensate for LG's vastly inferior
    hardware heat dispersal.
    
    Change-Id: I14960c757920d57445d52139aa025484999196b7
    Signed-off-by: Adam Farden <adam@farden.cz>

--- g3.mk
+++ g3.mk
@@ -206,9 +206,6 @@ PRODUCT_PACKAGES += \
     init.galbi.usb.sh \
     ueventd.g3.rc
 
-PRODUCT_PACKAGES += \
-    init.galbi.thermal_conf.sh
-
 # RIL symbols
 PRODUCT_PACKAGES += \
     liblge
@@ -218,8 +215,11 @@ PRODUCT_PACKAGES += \
     libstlport
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermanager
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
+    $(LOCAL_PATH)/configs/thermanager.xml:system/etc/thermanager.xml
 
 # USB
 PRODUCT_PACKAGES += \
