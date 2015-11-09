74a68cfb0d9b097298014356f807c76072baa591
Hendrik Hagendorn <git@finnq.de>
2015-11-09 22:08:17 +0100

    g3: Cleanup init
    Change-Id: Idb2b86f069e73fda7e91b8f6221c87af9ed4233f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,14 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-# Configuration scripts
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.galbi.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.galbi.bt.sh
-include $(BUILD_PREBUILT)
-
 # Init scripts
 
 include $(CLEAR_VARS)
@@ -44,50 +35,26 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi.class_core.sh
+LOCAL_MODULE            := init.galbi.sensor.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi.class_core.sh
+LOCAL_SRC_FILES         := etc/init.galbi.sensor.sh
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi.early_boot.sh
+LOCAL_MODULE            := init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi.early_boot.sh
+LOCAL_SRC_FILES         := etc/init.qcom.early_boot.sh
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi-sensor.sh
+LOCAL_MODULE            := init.qcom.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi-sensor.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi.syspart_fixup.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi.syspart_fixup.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi.usb.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi.usb.sh
+LOCAL_SRC_FILES         := etc/init.qcom.sh
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
