a63b2185a3e71c4b014405355c4a46a4ec77b90d
Adam Farden <adam@farden.cz>
2015-11-10 14:42:43 +0100

    g3: Use OSS TimeKeep service by Sony
    Works much better than QCOM proprietary blobs
    
    Change-Id: I50e72663169ee2907e248f634a9d1a4d3792c227
    Signed-off-by: Adam Farden <adam@farden.cz>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,6 +119,3 @@ BOARD_RIL_CLASS := ../../../device/lge/g3-common/ril/
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
