8e7d0bc57fdc2c40dc4bf87f2bdf0eb99b43f124
Hendrik Hagendorn <git@finnq.de>
2015-12-11 23:33:14 +0100

    g3: Fix setting network type
    I have no idea why LGs RIL is not responding without these three ints.
    Revert when we have M blobs.
    
    Since there's no instance of 'RadioCapability' in 'PhoneBase' the
    current RAF can't be retrieved.
    This is because 'RIL_REQUEST_GET_RADIO_CAPABILITY' ends in a timeout.
    Setting three integers in 'getRadioCapability's 'RILRequest' gets the
    RIL to respond with 'REQUEST_NOT_SUPPORTED'.
    
    Variants will need to opt in by setting ro.telephony.ril_class=LgeLteRIL
    in their respective device trees. This is left out of g3-common because
    some variants may need to further extend this class.
    
    Change-Id: Iecae7d4bfd1e412a0eb8492aae0a7a79dc33746a

---
+++ ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.telephony;
+
+import static com.android.internal.telephony.RILConstants.*;
+
+import android.content.Context;
+import android.os.Message;
+
+/**
+ * Custom Qualcomm RIL for G3
+ *
+ * {@hide}
+ */
+public class LgeLteRIL extends RIL implements CommandsInterface {
+    public LgeLteRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
+        super(context, preferredNetworkType, cdmaSubscription, null);
+    }
+
+    public LgeLteRIL(Context context, int preferredNetworkType,
+            int cdmaSubscription, Integer instanceId) {
+        super(context, preferredNetworkType, cdmaSubscription, instanceId);
+    }
+
+    @Override
+    public void getRadioCapability(Message response) {
+        RILRequest rr = RILRequest.obtain(
+                RIL_REQUEST_GET_RADIO_CAPABILITY, response);
+        rr.mParcel.writeInt(2); // can't be zero
+        rr.mParcel.writeInt(0); // random
+        rr.mParcel.writeInt(0); // random
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
+        send(rr);
+    }
+}
