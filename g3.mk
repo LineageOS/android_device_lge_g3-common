724f0cf8cec49dd918365425ad27d47f797c09c2
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-14 19:53:22 -0600

    audio: sync to caf
    * commonize mixer_paths.xml from copper LA.BF.1.1.3_rb1.5
    * remove audio_platform_info.xml
    
    Change-Id: I21ff6f2c2dd0a62174114091014e7b61cca51e60

--- g3.mk
+++ g3.mk
@@ -84,8 +84,8 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
