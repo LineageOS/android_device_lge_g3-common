cc3e54f259b9317c00c5fe13dc89683749effc05
Hendrik Hagendorn <git@finnq.de>
2016-01-14 20:49:00 +0100

    g3: Update GPS HAL
    * LA.BF.1.1.3-00810-8x74.0
    
    Change-Id: If3cb1ddad0da214edf445165403b6f716fc5bff9

--- gps/libloc_api_50001/Android.mk
+++ gps/libloc_api_50001/Android.mk
@@ -41,6 +41,7 @@ LOCAL_CFLAGS += \
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
+    $(TARGET_OUT_HEADERS)/libflp \
     $(LOCAL_PATH)
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
@@ -94,7 +95,8 @@ endif
 ## Includes
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    $(TARGET_OUT_HEADERS)/libloc_core
+    $(TARGET_OUT_HEADERS)/libloc_core \
+    $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_RELATIVE_PATH := hw
