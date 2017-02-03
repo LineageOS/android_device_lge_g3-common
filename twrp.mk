716ba6d993b85422f4dc2e22339215c691f55207
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-03 09:50:42 -0600

    g3-common: Add TWRP flags and fstab
    Add the necessary flags and fstab to optionally build TWRP
    Follow these steps to build TWRP
    
      * git clone https://github.com/LineageOS/android_external_busybox external/busybox
      * git clone https://github.com/omnirom/android_bootable_recovery bootable/recovery-twrp
      * source build/envsetup.sh
      * breakfast <device>
      * export WITH_TWRP=true
      * mka recoveryimage
    
    Change-Id: I687c3125a1c8bab0149cb6432b3f30b826aed6fb

---
+++ twrp.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += device/lge/g3-common/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
