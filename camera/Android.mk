5f0eb5468d20717506d471adcfdc1e96d8e8d5d0
Seth Shelnutt <Shelnutt2@gmail.com>
2017-08-26 17:24:40 -0400

    g3: Add missing shared/static libs to camera hal
    Change-Id: I97f798b521b0ef46f45660d651d3f5dd6c65af17

--- camera/Android.mk
+++ camera/Android.mk
@@ -5,9 +5,13 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libgui libutils libhidltransport android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect libbase
 
 LOCAL_C_INCLUDES += \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
@@ -15,4 +19,3 @@ LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
