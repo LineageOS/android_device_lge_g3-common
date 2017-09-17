40d90ce0c7feca04c3452865f86f22a091ca7560
Ethan Chen <intervigil@gmail.com>
2017-09-17 02:27:36 -0700

    g3-common: Convert lights HAL to native binderized HAL
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

--- g3.mk
+++ g3.mk
@@ -145,8 +145,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8974
+    android.hardware.light@2.0-service.g3
 
 # Location
 PRODUCT_PACKAGES += \
