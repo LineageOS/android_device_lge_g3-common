f5d420df137d52032ea114d2524405da21864c01
Arne Coucheron <arco68@gmail.com>
2017-10-10 00:06:30 +0200

    g3-common: Move libwcnss_qmi to vendor
    Change-Id: Iaff68497995b52f43c184832eaefc9dfe095e239

--- wcnss_lge/Android.mk
+++ wcnss_lge/Android.mk
@@ -24,6 +24,7 @@ LOCAL_CFLAGS += -Wall
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libwcnss_qmi
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
