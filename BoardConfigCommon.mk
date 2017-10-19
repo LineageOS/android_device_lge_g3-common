0c348bdf81c7c741582c48fa0bbacc9bfbab5073
ZjemCiKolege <frager1911@gmail.com>
2017-10-19 10:04:03 +0000

    g3-common: add BOARD_KERNEL_IMAGE_NAME
    Background: https://review.lineageos.org/#/c/192397/
    
    Change-Id: I706345849f0d62ddc47d608dda4a1cf534b9ade2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000
