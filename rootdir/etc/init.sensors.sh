ca53823b1a42059563a8d87a02ce2ccaf55e1d04
Dave Antoine <bryan2894@gmail.com>
2018-01-03 19:40:01 -0500

    init: move services to vendor
    Change-Id: I1bd870e18d69b9dc8b411cf3439e9af7a7141f55

--- rootdir/etc/init.sensors.sh
+++ rootdir/etc/init.sensors.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-if [[ ! -f /sns/cal/calb && -f /system/etc/sensors/sensor_def_variable.conf ]]; then
+if [[ ! -f /sns/cal/calb && -f /vendor/etc/sensors/sensor_def_variable.conf ]]; then
     rm /sns/cal/sns.reg
     touch /sns/cal/calb
 fi
