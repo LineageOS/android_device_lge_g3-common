1c16167e70c3d8555e515b4600167fd00ced7ef5
Dave Antoine <bryan2894@gmail.com>
2018-02-16 20:27:01 -0500

    g3-common: remove references to hostapd
    Change-Id: I9d8446c48c0c1ab1733d66128a7c0254ebd4a5bb

--- g3.mk
+++ g3.mk
@@ -241,8 +241,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     wificond \
-    hostapd \
-    hostapd_default.conf \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
