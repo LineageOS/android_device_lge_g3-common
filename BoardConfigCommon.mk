91fbd3ec21b6c7a2980d5d222f935da7ba7e0b27
dianlujitao <dianlujitao@lineageos.org>
2018-08-12 22:21:50 +0200

    g3-common: Nuke BOARD_USES_QC_TIME_SERVICES flag
     * Any pre-O framework did exact the same thing as TimeService.apk
       when the flag is enabled, as we always ship TimeService.apk,
       there's no reason to keep the flag, and the framework stuff is
       not ported to O.
    
    Change-Id: I74abc65468e983732bccde571003235891379463

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,8 +126,5 @@ include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/lge/g3-common/sepolicy
 
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Shipping API level (for CTS backward compatibility)
 PRODUCT_SHIPPING_API_LEVEL := 19
