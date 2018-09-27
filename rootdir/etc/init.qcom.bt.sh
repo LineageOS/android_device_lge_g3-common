c11f7ffae9060d14d28642b78cf10547dca83f6e
dianlujitao <dianlujitao@lineageos.org>
2018-09-27 23:18:40 +0200

    g3-common: Update bluetooth properties for new vendor prefixes
    Change-Id: Ib434f0b6d8a1929e9333839f1cd4d66634565828

--- rootdir/etc/init.qcom.bt.sh
+++ rootdir/etc/init.qcom.bt.sh
@@ -2,9 +2,9 @@
 
 if [ -f /vendor/bin/hci_qcomm_init ]; then
     setprop ro.qualcomm.bt.hci_transport smd
-    setprop bluetooth.status off
+    setprop vendor.bluetooth.status off
     /vendor/bin/hci_qcomm_init -b `cat /data/misc/bluetooth/bdaddr`
-    setprop bluetooth.status on
+    setprop vendor.bluetooth.status on
 fi
 
 exit 0
