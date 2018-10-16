47c1ddb7b3b3c366561021e983e88755845a9ffa
firebird11 <hbgassel@gmail.com>
2018-10-16 21:32:36 +0200

    g3-common: firmware and symlinks to /vendor
    Change-Id: I9377061801c7792f881b024695f57f8a452b1093

--- Android.mk
+++ Android.mk
@@ -30,7 +30,7 @@ endef
 DXHDCP2_IMAGES := \
     dxhdcp2.b00 dxhdcp2.b01 dxhdcp2.b02 dxhdcp2.b03 dxhdcp2.mdt
 
-DXHDCP2_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(DXHDCP2_IMAGES))
+DXHDCP2_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(DXHDCP2_IMAGES))
 $(DXHDCP2_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "dxhdcp2 firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -42,7 +42,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(DXHDCP2_SYMLINKS)
 KEYMASTER_IMAGES := \
     keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
 
-KEYMASTER_SYMLINKS :=$(addprefix $(TARGET_OUT_ETC)/firmware/,$(KEYMASTER_IMAGES))
+KEYMASTER_SYMLINKS :=$(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(KEYMASTER_IMAGES))
 $(KEYMASTER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -54,7 +54,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(KEYMASTER_SYMLINKS)
 MLSERVERAPP_IMAGES := \
     mlserverapp.b00 mlserverapp.b01 mlserverapp.b02 mlserverapp.b03 mlserverapp.mdt
 
-MLSERVERAPP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(MLSERVERAPP_IMAGES))
+MLSERVERAPP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(MLSERVERAPP_IMAGES))
 $(MLSERVERAPP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "mlserverapp firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -66,7 +66,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(MLSERVERAPP_SYMLINKS)
 TQS_IMAGES := \
     tqs.b00 tqs.b01 tqs.b02 tqs.b03 tqs.mdt
 
-TQS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(TQS_IMAGES))
+TQS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(TQS_IMAGES))
 $(TQS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "tqs firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -78,7 +78,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(TQS_SYMLINKS)
 WCD9320_IMAGES := \
     wcd9320_anc.bin wcd9320_mad_audio.bin wcd9320_mbhc.bin
 
-WCD9320_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/wcd9320/,$(WCD9320_IMAGES))
+WCD9320_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/wcd9320/,$(WCD9320_IMAGES))
 $(WCD9320_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "wcd9320 firmware link: $@"
 	@mkdir -p $(dir $@)
