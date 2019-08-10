2b31f72a0fca8ecbfd041d146484cdd71ed32900
Ethan Chen <intervigil@gmail.com>
2019-08-10 14:04:56 +0200

    g3-common: Add Vibrator HAL in place of CMHW implementation
    Change-Id: I19d4f916d807301ec30b882bfdeaf76715a041c7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,10 +71,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
-# CMHW
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/lineagehw/
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap_to_wake"
-
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_LEGACY_HW_DISK_ENCRYPTION := true
@@ -109,7 +105,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 # Power
 TARGET_HAS_LEGACY_POWER_STATS := true
 TARGET_HAS_NO_WLAN_STATS := true
-TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap_to_wake"
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
