ccf26212947979ec6d2584e6b4f12e231a215cab
Bruno Martins <bgcngm@gmail.com>
2019-10-06 20:15:30 +0200

    g3-common: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- g3.mk
+++ g3.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwpa_client \
     wpa_supplicant \
