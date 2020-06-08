768fc9471bd049721c391a275afb25dc062e0bc9
Aleksander <johnnylittleplanet@gmail.com>
2020-06-08 20:00:58 +0200

    g3-common: Add and move gps configs to gps/etc
     * flp, izat, sap are taken from D855v30o
    
    Change-Id: I4cd03bac73cc73cfb0080b9c74c063b33253f07f

--- g3.mk
+++ g3.mk
@@ -171,8 +171,10 @@ PRODUCT_PACKAGES += \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # Media
 PRODUCT_COPY_FILES += \
