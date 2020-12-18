80b06d5b6120ac9167707322eb1d87ab986cb7f8
Bruno Martins <bgcngm@gmail.com>
2020-12-18 16:16:57 +0100

    g3-common: Kill legacy keystore
    Legacy credential hash support was obsolete and got removed completely.
    From now on all the devices need to rely on gatekeeper, which
    in case of legacy devices should be the software implementation.
    
    This fixes setting a lockscreen PIN/password/pattern.
    
    Ref: https://android.googlesource.com/platform/frameworks/base/+/a758727c963948ae07e97c977a63b882216e8971
    
    Change-Id: I020d1807db13b73f9990ac3704eadba49b65ee5c

--- g3.mk
+++ g3.mk
@@ -158,10 +158,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Keystore
-PRODUCT_PACKAGES += \
-    keystore.msm8974
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
